7de8574c88440a594f6693cd43a0966a0f0bb5d2
o-jill <nobaokix@gmail.com>
2022-03-06 10:35:52 +0900

    added help page.

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,26 @@ async fn main() {
 }
 
 fn app() -> Router {
-    Router::new().route("/", get(handler))
+    Router::new()
+        .route("/", get(handler))
+        .route("/help", get(help))
+}
+
+async fn help() -> axum::response::Html<&'static str> {
+    axum::response::Html(
+        "<html><head><title>help - sfenimageserver -</title></head>\
+        <body><h1>sfenimageserver<h1>\
+        <h2>options</h2>\
+        <ul><li>sfen<br>sfen text. this must be given.<br>\
+        ex. \"lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1\"\
+        <li>sname<br>sente's name.
+        <li>gname<br>gote's name.
+        <li>title<br>title.
+        <li>turn<br>turn. b, w, fb, fw or d.
+        <li>image<br>svg or png.
+        </ul>
+        </body></html>",
+    )
 }
 
 async fn handler(Query(params): Query<Params>) -> (HeaderMap, Vec<u8>) {
