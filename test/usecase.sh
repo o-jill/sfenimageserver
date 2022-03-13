0c9e38d44c23e60dcc28e1e4cb47d1fe7e62e484
o-jill <nobaokix@gmail.com>
2022-03-13 17:47:09 +0900

    fixed command option.

--- test/usecase.sh
+++ test/usecase.sh
@@ -10,6 +10,6 @@ curl http://127.0.0.1:7582/help > help.html
 
 curl http://127.0.0.1:7582/?sfen=lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL+b+-+1 > test01.svg
 
-curl http://127.0.0.1:7582/?sfen=lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL+b+-+1&image=png > test01.png
+curl http://127.0.0.1:7582/?sfen=lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL+b+-+1\&image=png > test01.png
 
 pkill sfenimageserver
