bee7c514a7fe3ba573605b1631eed05621d6140a
o-jill <nobaokix@gmail.com>
2022-03-16 11:28:03 +0900

    added some usecase.

--- test/usecase.sh
+++ test/usecase.sh
@@ -10,6 +10,12 @@ curl http://127.0.0.1:7582/help > help.html
 
 curl http://127.0.0.1:7582/?sfen=lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL+b+-+1 > test01.svg
 
+curl http://127.0.0.1:7582/?sfen=lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL+b+-+1\&lm=0093FU > test02.svg
+
+curl http://127.0.0.1:7582/?sfen=lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL+b+-+1\&lm=13 > test03.svg
+
+curl http://127.0.0.1:7582/?sfen=lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL+b+-+1\&sname=o-jill\&gname=%e3%81%a2%e3%82%8b > test04.svg
+
 curl http://127.0.0.1:7582/?sfen=lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL+b+-+1\&image=png > test01.png
 
 pkill sfenimageserver
