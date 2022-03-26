a4ec08a2da62722b8fc47f1ef99504ebb9272ae5
o-jill <nobaokix@gmail.com>
2022-03-26 22:41:35 +0900

    added title.

--- test/usecase.sh
+++ test/usecase.sh
@@ -12,7 +12,7 @@ curl http://127.0.0.1:7582/?sfen=lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1
 
 curl http://127.0.0.1:7582/?sfen=lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL+b+-+1\&lm=0093FU > test02.svg
 
-curl http://127.0.0.1:7582/?sfen=lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL+b+-+1\&lm=13 > test03.svg
+curl http://127.0.0.1:7582/?sfen=lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL+b+-+1\&lm=13\&title=hello+world!! > test03.svg
 
 curl http://127.0.0.1:7582/?sfen=lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL+b+-+1\&sname=o-jill\&gname=%e3%81%a2%e3%82%8b > test04.svg
 
